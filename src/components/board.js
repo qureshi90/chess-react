7933e80c471fc92e0fb4e1822c6db112f42f25d6
Waqar Qureshi <waqarqureshi90@gmail.com>
2020-12-31 20:49:01 +0500

    chess board designed

---
+++ src/components/board.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Board() {
+  return (
+    <>
+      <table border="1" cellSpacing="0">
+        <tbody className="board">
+          {Array.from({ length: 8 }).map((_, rowId) => (
+            <tr key={rowId}>
+              {Array.from({ length: 8 }).map((_, colId) => (
+                <td
+                  key={colId}
+                  className={ rowId % 2 === 1
+                    ? colId % 2 === 0
+                      ? "cell-color table-cell"
+                      : "table-cell"
+                    : colId % 2 === 1 
+                      ? "cell-color table-cell" 
+                      : "table-cell"
+                  }
+                >
+                  {rowId}
+                  {colId}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default Board;
